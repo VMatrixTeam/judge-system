2b0422f14c0e834d9f4de8fc0ae68233d3af909c
Howard Lau <howardlau1999@hotmail.com>
2021-09-27 17:00:43 +0800

    build: two-stage build

--- hack/build.sh
+++ hack/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 mkdir -p build
+mkdir -p build/rabbitmqc-build
+pushd build/rabbitmqc-build && cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../librabbitmq -G"Ninja" ../../ext/rabbitmq-c && cmake --build . && ninja install && popd
+rm -rf build/rabbitmqc-build
 echo "Building Judge System DEBUG VERSION"
-pushd build && cmake -DCPR_BUILD_TESTS=OFF -DCPR_BUILD_TESTS_SSL=OFF -DBUILD_ENTRY=ON -G"Ninja" $@ .. && cmake --build . && popd
+pushd build && cmake -DRabbitmqc_DIR=$(pwd)/librabbitmq -DCPR_BUILD_TESTS=OFF -DCPR_BUILD_TESTS_SSL=OFF -DBUILD_ENTRY=ON -G"Ninja" $@ .. && cmake --build . && popd
