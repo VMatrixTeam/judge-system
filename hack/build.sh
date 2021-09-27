87caddd8eebf94c6cf4394c0674d2c977a1f9943
Howard Lau <howardlau1999@hotmail.com>
2021-09-28 00:13:36 +0800

    hack: update build script

--- hack/build.sh
+++ hack/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 mkdir -p build
-mkdir -p build/rabbitmqc-build
-pushd build/rabbitmqc-build && cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../librabbitmq -G"Ninja" ../../ext/rabbitmq-c && cmake --build . && ninja install && popd
-rm -rf build/rabbitmqc-build
+mkdir -p build/ext/rabbitmq-c
+pushd build/ext/rabbitmq-c && cmake -DCMAKE_INSTALL_PREFIX=$(pwd) -G"Ninja" ../../../ext/rabbitmq-c && cmake --build . && ninja install && popd
 echo "Building Judge System DEBUG VERSION"
-pushd build && cmake -DRabbitmqc_DIR=$(pwd)/librabbitmq -DCPR_BUILD_TESTS=OFF -DCPR_BUILD_TESTS_SSL=OFF -DBUILD_ENTRY=ON -G"Ninja" $@ .. && cmake --build . && popd
+pushd build && cmake -DRabbitmqc_DIR=$(pwd)/ext/rabbitmq-c -DCPR_BUILD_TESTS=OFF -DCPR_BUILD_TESTS_SSL=OFF -DBUILD_ENTRY=ON -G"Ninja" $@ .. && cmake --build . && popd
